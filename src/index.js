b47afbd5396f2e98f5bdecc81ca1c7ca21d95536
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-14 20:45:49 +0300

    add

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import qwer from './js/third';
 
 console.log(qwer.secondNum);
 
+const user = {
+    name: 'mango',
+    age: 18
+};
+
+const userJson = JSON.stringify(user);
+console.log(userJson);
+
+const userParse = JSON.parse(userJson);
+console.log(userParse);
+
+localStorage.setItem('user-data', userJson);
+
+const dataLs = localStorage.getItem('user-data');
+const parseData = JSON.parse(dataLs);
+console.log(parseData);
+
+localStorage.removeItem('user-data');
